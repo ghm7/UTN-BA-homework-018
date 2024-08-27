aee5a34fcac384de3c47d2f62f92100785fe4a51
Gabriel Pozzi <pozzisgnosis@outlook.com>
2024-08-26 23:45:29 -0300

    Fix gallery bug

--- client/src/components/GalleryGrid/GalleryGrid.component.tsx
+++ client/src/components/GalleryGrid/GalleryGrid.component.tsx
@@ -1,44 +1,15 @@
-import { ReactElement, useEffect, useRef } from 'react';
+import { ReactElement } from 'react';
 
 import { images } from '../../data/galleryData';
 import { Link } from 'react-router-dom';
 
 import './GalleryGrid.styles.css';
 
-type IntersectionObserverCallback = (
-  entries: IntersectionObserverEntry[],
-  observer: IntersectionObserver
-) => void;
-
 export default function GalleryGrid(): ReactElement {
-  const figuresRef = useRef([]);
-
-  useEffect(() => {
-    const intersectionCallback: IntersectionObserverCallback = (entries) => {
-      for (const entry of entries) {
-        if (entry.isIntersecting) {
-          entry.target.classList.add('show');
-        }
-      }
-    };
-
-    const observer = new IntersectionObserver(intersectionCallback);
-
-    for (const figure of figuresRef.current) {
-      observer.observe(figure);
-    }
-  }, []);
-
   return (
     <div className="grid grid-cols-3">
       {images.map(({ gallery }, index) => (
-        <Link
-          to={gallery[0].id}
-          key={index}
-          className="figure"
-          //@ts-expect-error Type HTMLAnchorElement | null is not asignable to type never
-          ref={(itemRef) => (figuresRef.current[index] = itemRef)}
-        >
+        <Link to={gallery[0].id} key={index} className="figure">
           <figure className="grid grid-cols-1 grid-rows-1 justify-items-center overflow-hidden">
             <img
               src={gallery[0].path}
