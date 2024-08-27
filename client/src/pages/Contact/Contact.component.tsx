af6127dc0258f773d150cf37de1276fd3713233c
Gabriel Pozzi <pozzisgnosis@outlook.com>
2024-08-26 23:20:45 -0300

    Add sendEmail functionality

--- client/src/pages/Contact/Contact.component.tsx
+++ client/src/pages/Contact/Contact.component.tsx
@@ -1,24 +1,52 @@
-import { ReactElement } from 'react';
+import { ReactElement, useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 import { Facebook, Instagram, Mail } from 'react-feather';
 
 import './Contact.styles.css';
 
 export default function Contact(): ReactElement {
+  const form = useRef<HTMLInputElement>(null);
+
+  const sendEmail = (e: Event) => {
+    e.preventDefault();
+
+    emailjs
+      // @ts-expect-error Argument of type
+      .sendForm('service_lmej4ka', 'template_j0joswx', form.current, {
+        publicKey: 'lqqxTAPA37BE1RdW2',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        }
+      );
+  };
+
   return (
     <div className="flex justify-center gap-[12rem] remaining-space">
       <div className="flex flex-col justify-center items-center">
-        <form action="post" className="w-[400px] flex flex-col gap-6">
+        <form
+          // @ts-expect-error ...is not assignable to type...
+          ref={form}
+          action="post"
+          className="w-[400px] flex flex-col gap-6"
+          // @ts-expect-error
+          onSubmit={sendEmail}
+        >
           <input
             type="text"
             name="name"
             placeholder="Nombre"
-            id="name"
+            id="user_name"
             className="w-[100%] bg-primaryBg border-b-2 border-white box-border p-2"
           />
           <input
             type="email"
-            name="email"
+            name="user_email"
             placeholder="Email"
             id="email"
             className="w-[100%] bg-primaryBg border-b-2 border-white box-border p-2"
